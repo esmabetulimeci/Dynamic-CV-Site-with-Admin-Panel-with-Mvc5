46b292ee6b963567dff894a12edcdab5fc1255ab
esmaimeci <imeciesma@gmail.com>
2023-09-28 17:06:42 +0300

    Proje dosyası ekle.

---
+++ MvcCV/Controllers/DefaultController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+
+namespace MvcCV.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCVEntities db = new DbCVEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimler.ToList();
+            return PartialView(deneyimler);
+        }
+
+        public PartialViewResult Egitim()
+        {
+            var egitimler = db.TblEgitim.ToList();
+            return PartialView(egitimler);
+        }
+
+        public PartialViewResult Yetenekler()
+        {
+            var yetenekler = db.TblYetenek.ToList();
+            return PartialView(yetenekler);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+            var hobilerim = db.TblHobilerim.ToList();
+            return PartialView(hobilerim);
+        }
+
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifikalar = db.TblSertifikalarım.ToList();
+            return PartialView(sertifikalar);
+        }
+        [HttpGet]
+        public PartialViewResult Iletişim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Iletişim(TblIletisim t)
+        {
+            t.Traih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblIletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ MvcCV/Repository/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcCV.Models.Entity;
+
+namespace MvcCV.Repository
+{
+    public class GenericRepository<T> where T : class ,new()
+    {
+          DbCVEntities db = new DbCVEntities();
+          
+        public List<T> List() { 
+          
+            return db.Set<T>().ToList();
+        }
+
+        public void TAdd(T p)
+        {
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+
+        public void TDelete(T p)
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+
+        public T TGet(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public void TUpdate(T p)
+        {
+            db.SaveChanges();
+        }
+    }
+}
